b4d718dab6d287c3529846a19b87b8ee406fd628
mimir-d <mimir-d@users.noreply.github.com>
2022-08-30 01:30:32 +0100

    add file schema; extract testStepArtifact
    - add file json schema and metadata dependency
    - extract testStepArtifact from main file
    - add unit test
    
    ==
    
    - testing:
    ```
    > go test ./...
    ?       github.com/opencomputeproject/test_and_validation/json_validator        [no test files]
    ok      github.com/opencomputeproject/test_and_validation/json_validator/pkg/schema_validator   0.033s
    ```
    
    Signed-off-by: mimir-d <mimir-d@users.noreply.github.com>

--- validators/spec_validator/pkg/schema_validator/validator_test.go
+++ validators/spec_validator/pkg/schema_validator/validator_test.go
@@ -215,6 +215,26 @@ func TestStepErrorSimple(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestStepFileSimple(t *testing.T) {
+	json := `{
+		"testStepArtifact": {
+			"testStepId": "1",
+			"file": {
+				"displayName": "mem_cfg_log",
+				"uri": "file:///root/mem_cfg_log",
+				"description": "DIMM configuration settings.",
+				"contentType": "text/plain",
+				"isSnapshot": false
+			}
+		},
+		"sequenceNumber": 1,
+		"timestamp": "2021-10-19T22:59:20+00:00"
+	}`
+
+	err := validateString(t, json)
+	require.NoError(t, err)
+}
+
 func validateString(t *testing.T, json string) error {
 	const schema string = "../../../../json_spec/output/spec.json"
 
