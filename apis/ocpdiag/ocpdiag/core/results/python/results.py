2fa575f2b30dfeedd185a00e0cce39b6cfb8b76e
Troy Wang <troywang@google.com>
2022-09-21 16:47:50 -0700

    Leveraging return values of absl flags `DEFINE_*` methods.
    Signed-off-by: Troy Wang <troywang@google.com>
    PiperOrigin-RevId: 475956464

--- apis/ocpdiag/ocpdiag/core/results/python/results.py
+++ apis/ocpdiag/ocpdiag/core/results/python/results.py
@@ -21,29 +21,29 @@ from absl import app
 from absl import flags
 
 # Define the flags with identical names to corresponding c++ flags.
-flags.DEFINE_boolean(
+_OCPDIAG_COPY_RESULTS_TO_STDOUT = flags.DEFINE_boolean(
     'ocpdiag_copy_results_to_stdout',
     True,
     'Prints human-readable result artifacts to stdout in addition to default '
     'output',
     allow_hide_cpp=True)
-flags.DEFINE_string(
+_OCPDIAG_RESULTS_FILEPATH = flags.DEFINE_string(
     'ocpdiag_results_filepath',
     '',
     'Fully-qualified file path where binary-proto result data gets written.',
     allow_hide_cpp=True)
-flags.DEFINE_string(
+_MACHINE_UNDER_TEST = flags.DEFINE_string(
     'machine_under_test',
     'local',
     'Machine under test. If the test binary is running on the same machine as '
     'the machine under test, just keep the default \"local\".',
     allow_hide_cpp=True)
-flags.DEFINE_boolean(
+_OCPDIAG_STRICT_REPORTING = flags.DEFINE_boolean(
     'ocpdiag_strict_reporting',
     True, 'Whether to require a global devpath to be reported in '
     'third_party.ocpdiag.results_pb.HardwareInfo',
     allow_hide_cpp=True)
-flags.DEFINE_boolean(
+_ALSOLOGTOOCPDIAGRESULTS = flags.DEFINE_boolean(
     'alsologtoocpdiagresults',
     False,
     'If set to true, ABSL and Ecclesia logger will be also direct to OCPDiag '
@@ -54,11 +54,11 @@ flags.DEFINE_boolean(
 # Register the flags to be set on init.
 def _SetPybindFlags():
   SetResultsLibFlags(
-      flags.FLAGS.ocpdiag_copy_results_to_stdout,
-      flags.FLAGS.ocpdiag_results_filepath,
-      flags.FLAGS.machine_under_test,
-      flags.FLAGS.alsologtoocpdiagresults,
-      flags.FLAGS.ocpdiag_strict_reporting,
+      _OCPDIAG_COPY_RESULTS_TO_STDOUT.value,
+      _OCPDIAG_RESULTS_FILEPATH.value,
+      _MACHINE_UNDER_TEST.value,
+      _ALSOLOGTOOCPDIAGRESULTS.value,
+      _OCPDIAG_STRICT_REPORTING.value,
   )
 
 
