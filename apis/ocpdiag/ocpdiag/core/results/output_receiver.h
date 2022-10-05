5e50007cb28867df02404ba04477caed53afa29d
Meltan Team <meltan-core-team@google.com>
2022-10-05 10:15:33 -0700

    INTERNAL_CHANGE
    PiperOrigin-RevId: 479077168

--- apis/ocpdiag/ocpdiag/core/results/output_receiver.h
+++ apis/ocpdiag/ocpdiag/core/results/output_receiver.h
@@ -7,52 +7,55 @@
 #ifndef OCPDIAG_CORE_RESULTS_OUTPUT_RECEIVER_H_
 #define OCPDIAG_CORE_RESULTS_OUTPUT_RECEIVER_H_
 
+#include <optional>
 #include <string>
 
-#include "absl/flags/reflection.h"
+#include "ocpdiag/core/results/internal/logging.h"
 #include "ocpdiag/core/results/output_model.h"
+#include "ocpdiag/core/results/recordio_iterator.h"
 #include "ocpdiag/core/results/results.pb.h"
 
 namespace ocpdiag::results {
 
-// A helper class for unit tests to consume OCPDiag output artifacts.
+// A helper class for unit tests to consume OCPDiag output artifacts. It collects
+// any artifacts written to artifact_writer.
 //
-// The current implementation consumes recordIO output produced by the TestRun
-// library, but future implementations may decide to do this differently without
-// needing to change the unit testing API.
+// This class is not thread-safe.
 class OutputReceiver {
  public:
-  using OutputCallback =
-      std::function<bool(ocpdiag::results_pb::OutputArtifact)>;
+  // The iterator allows us to be used as a container in range-based for loops.
+  using const_iterator =
+      RecordIoIterator<ocpdiag::results_pb::OutputArtifact>;
 
-  // Begins capturing OCPDiag results output, or crashes with a stack trace if
-  // it's unable to for some reason.
+  // Constructs a new output receiver.
   OutputReceiver();
-  ~OutputReceiver() { Close(); }
 
-  // Returns the output that was received. After calling this the first time,
-  // the results never change, so only call this after the OCPDiag test has
-  // finished.
+  // We will collect all output artifacts written to this object.
+  internal::ArtifactWriter &artifact_writer() const {
+    return *artifact_writer_;
+  }
+
+  // Returns all the output artifacts in a structured data model. The
+  // results are cached after the first call, so you should only call this after
+  // the test has run to completion.
   //
   // This will store all the output protobufs in memory at once. If that is a
-  // problem, consider Iterate() to only hold one proto in memory at a time.
+  // problem, consider using an iterator which holds only one proto in memory
+  // at a time.
   const TestRunOutput &model();
 
-  // Iterates over all results, executing the callback on each one.
-  // This method avoids storing all output in memory at once.
-  void Iterate(const OutputCallback &callback);
-
-  // Closes down the receiver. This does not need to be called explicitly,
-  // except for in Python wrappers.
-  void Close();
+  // Returns an iterator to the raw output. Don't use these directly, use them
+  // indirectly by using this object as a container in range-based for loops.
+  //
+  // Example:
+  //   for (auto artifact : output_receiver) {...}
+  const_iterator begin() const;
+  const_iterator end() const;
 
  private:
-  // Consumes the test run output and populates a new model.
-  TestRunOutput ConsumeOutputOrDie();
-
-  std::unique_ptr<absl::FlagSaver> flag_saver_;
-  const std::string filename_;
-  std::optional<TestRunOutput> output_;
+  const std::string file_path_;
+  std::unique_ptr<internal::ArtifactWriter> artifact_writer_;
+  std::optional<TestRunOutput> model_;
 };
 
 }  // namespace ocpdiag::results
