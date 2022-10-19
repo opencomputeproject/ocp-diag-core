581024a88fa53c4fb577b432c630c0a1134bb852
Meltan Team <meltan-core-team@google.com>
2022-10-19 13:31:33 -0700

    INTERNAL_CHANGE
    PiperOrigin-RevId: 482289594

--- apis/ocpdiag/ocpdiag/core/examples/simple/simple_lib_test.cc
+++ apis/ocpdiag/ocpdiag/core/examples/simple/simple_lib_test.cc
@@ -35,7 +35,7 @@ using ::ocpdiag::results_pb::OutputArtifact;
 TEST(TestAddAllMeasurementTypes, CheckAll) {
   results::OutputReceiver output;
   {
-    results::TestRun test_run("test_run");
+    results::TestRun test_run("Test", output.artifact_writer());
     test_run.StartAndRegisterInfos({});
     auto step = test_run.BeginTestStep("fake_step");
 
@@ -44,13 +44,12 @@ TEST(TestAddAllMeasurementTypes, CheckAll) {
 
   // Check measurements.
   std::vector<Measurement> measurements;
-  output.Iterate([&](OutputArtifact artifact) {
+  for (OutputArtifact artifact : output) {
     if (artifact.has_test_step_artifact() &&
         artifact.test_step_artifact().has_measurement()) {
       measurements.push_back(artifact.test_step_artifact().measurement());
     }
-    return true;
-  });
+  }
 
   std::vector<std::string> expected_measurements{
       // Null measurements.
