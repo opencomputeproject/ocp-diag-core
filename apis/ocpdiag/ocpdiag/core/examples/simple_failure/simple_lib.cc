b3cb9add60203f298bccfa4fe2abe36e1036ee5e
Meltan Team <meltan-core-team@google.com>
2022-10-31 18:22:48 -0700

    Measurements that violate range limits or valid value constraints now cause test errors. The invalid measurements are still collected, but an error is logged which causes the test to fail. Callers can optionally abort the test upon receiving such errors, by checking the boolean return value and responding appropriately.
    PiperOrigin-RevId: 485196303

--- apis/ocpdiag/ocpdiag/core/examples/simple_failure/simple_lib.cc
+++ apis/ocpdiag/ocpdiag/core/examples/simple_failure/simple_lib.cc
@@ -15,7 +15,7 @@ using ::ocpdiag::results::TestStep;
 using ::ocpdiag::results_pb::MeasurementElement;
 using ::ocpdiag::results_pb::MeasurementInfo;
 
-constexpr int32_t kProtoValueTypeCnt = 5;
+constexpr int32_t kProtoValueTypeCnt = 2;
 
 void AddMeasurementWithRange(TestStep* step, std::string name, Value val,
                              Value max, Value min, const HwRecord* hwRecord) {
@@ -50,47 +50,22 @@ void AddAllMeasurementTypes(TestStep* step, const HwRecord* hwRecord) {
   std::vector<Value> vals(kProtoValueTypeCnt), maximums(kProtoValueTypeCnt),
       minimums(kProtoValueTypeCnt);
 
-  // Null value.
-  names[0] = "null-measurement";
-  vals[0].set_null_value(google::protobuf::NULL_VALUE);
-  maximums[0].set_null_value(google::protobuf::NULL_VALUE);
-  minimums[0].set_null_value(google::protobuf::NULL_VALUE);
-
   // Number value.
-  names[1] = "number-measurement";
-  vals[1].set_number_value(1.23);
-  maximums[1].set_number_value(2.34);
-  minimums[1].set_number_value(0.12);
+  names[0] = "number-measurement";
+  vals[0].set_number_value(1.23);
+  maximums[0].set_number_value(2.34);
+  minimums[0].set_number_value(0.12);
 
   // String value.
-  names[2] = "string-measurement";
-  vals[2].set_string_value("version-1.23");
-  maximums[2].set_string_value("version-2.34");
-  minimums[2].set_string_value("version-0.12");
-
-  // Boolean value.
-  names[3] = "boolean-measurement";
-  vals[3].set_bool_value(false);
-  maximums[3].set_bool_value(true);
-  minimums[3].set_bool_value(true);
-
-  // List Value.
-  names[4] = "list-measurement";
-  google::protobuf::ListValue* list_val = vals[4].mutable_list_value();
-  Value val_key1(vals[1]), val_key2(maximums[1]);
-  *list_val->add_values() = val_key1;
-  *list_val->add_values() = val_key2;
-  *maximums[4].mutable_list_value() = *list_val;
-  *minimums[4].mutable_list_value() = *list_val;
+  names[1] = "string-measurement";
+  vals[1].set_string_value("version-1.23");
+  maximums[1].set_string_value("version-2.34");
+  minimums[1].set_string_value("version-0.12");
 
   // Send measurements.
   for (int i = 0; i < kProtoValueTypeCnt; i++) {
-    // Range for lists, null, or bool doesn't make sense.
-    if (names[i] != "list-measurement" && names[i] != "null-measurement" &&
-        names[i] != "boolean-measurement") {
-      AddMeasurementWithRange(step, names[i], vals[i], maximums[i], minimums[i],
-                              hwRecord);
-    }
+    AddMeasurementWithRange(step, names[i], vals[i], maximums[i], minimums[i],
+                            hwRecord);
     AddMeasurementWithValidValues(
         step, names[i], vals[i],
         absl::Span<const Value>{vals[i], maximums[i], minimums[i]}, hwRecord);
