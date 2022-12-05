d70c5b1b3d294ad98f3baffb1ef5a8628f2717dc
Troy Wang <troywang@google.com>
2022-12-05 07:48:06 -0800

    Updated Ecclesia dep to the latest version.
    Signed-off-by: Troy Wang <troywang@google.com>
    PiperOrigin-RevId: 493005585

--- apis/ocpdiag/ocpdiag/core/testing/file_utils.cc
+++ apis/ocpdiag/ocpdiag/core/testing/file_utils.cc
@@ -6,17 +6,32 @@
 
 #include "ocpdiag/core/testing/file_utils.h"
 
+#include <cstdlib>
+#include <filesystem>  //
 #include <fstream>
 #include <sstream>
 
 #include "google/protobuf/text_format.h"
+#include "absl/log/check.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 
 namespace ocpdiag::testutils {
 
 constexpr const char* kSrcTestDir = "TEST_SRCDIR";
 constexpr const char* kSrcWorkspace = "TEST_WORKSPACE";
 constexpr absl::string_view kGoogleWorkspace = "google3";
+
+std::string MkTempFileOrDie(absl::string_view modifier) {
+  // Try several options for a temporary filename until a suitable one is found.
+  // Note that the tempdir may be different depending on environment.
+  std::string path = std::getenv("TEST_TMPDIR");
+  if (path.empty()) path = std::getenv("TMPDIR");
+  if (path.empty()) path = std::filesystem::temp_directory_path();
+  path += absl::StrFormat("/ocpdiag_%s_tempfile_XXXXXX", modifier);
+  CHECK_NE(mkstemp(path.data()), -1) << "Cannot make temp file path";
+  return path;
+}
 
 std::string GetDataDependencyFilepath(absl::string_view file) {
   const absl::string_view source_dir = getenv(kSrcTestDir);
