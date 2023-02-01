7f7e00562fac2f6f127723bf210f59580642777b
Dylan Hawkes <dthawkes@google.com>
2023-02-01 01:26:37 +0000

    Manually exported a new version of the OCP results library. Internal Reference: 506157018
    Signed-off-by: Dylan Hawkes <dthawkes@google.com>

--- apis/ocpdiag/ocpdiag/core/examples/noop/noop.cc
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Google LLC
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-
-int main(int argc, char* argv[]) { return 0; }
--- apis/ocpdiag/third_party/pybind11_abseil/status.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 Google LLC
-#
-# Use of this source code is governed by an MIT-style
-# license that can be found in the LICENSE file or at
-# https://opensource.org/licenses/MIT.
-
-"""Python wrapper for pybind11_abseil pybind extension."""
-
-from pybind11_abseil.status import *  # pylint:disable=wildcard-import
