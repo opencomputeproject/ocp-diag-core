f6ea0d81661abdc7c92342c99db7b3afd5cdbda5
mimir-d <mimir-d@users.noreply.github.com>
2023-06-10 13:36:38 +0100

    Move api/c++ to ocp-diag-core-cpp
    - incl relevant ci scripts
    Signed-off-by: mimir-d <mimir-d@users.noreply.github.com>

--- ci_scripts/run_ocpdiag_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-cd apis/c++
-bazel build ...
-bazel test ... --test_output=errors
--- ci_scripts/validate_ocpdiag_examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-cd apis/c++
-OUTPUT="$(bazel run //ocpdiag/core/examples/full_spec)"
-
-cd ../../validators/spec_validator
-echo "$OUTPUT" | go run . -schema ../../json_spec/output/root.json -
